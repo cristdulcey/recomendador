d6c7517770c486e84bcecefece5ec491e15f786c
laura <laespaviza@gmail.com>
2020-07-29 17:18:59 -0500

    crear modelos :client, city y supervisor

--- persons/admin.py
+++ persons/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from persons.models import Client, Supervisor, City
+
+
 # Register your models here.
+
+@admin.register(Client)
+class AdminClient(admin.ModelAdmin):
+    pass
+
+@admin.register(Supervisor)
+class AdminSupervisor(admin.ModelAdmin):
+    pass
+
+@admin.register(City)
+class AdminCity(admin.ModelAdmin):
+    pass
