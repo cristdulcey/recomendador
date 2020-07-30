e1762d9f6728bf359aa7370f344e947be9c4c0a7
laura <laespaviza@gmail.com>
2020-07-29 21:27:42 -0500

    cambios modelos general y admin de apps

--- companies/admin.py
+++ companies/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from companies.models import Company, BranchCompany, BranchServices, Qualification
+
+
+@admin.register(Company)
+class AdmniCompany(admin.ModelAdmin):
+    pass
+
+@admin.register(BranchCompany)
+class AdmniBranchCompany(admin.ModelAdmin):
+    pass
+
+@admin.register(BranchServices)
+class AdmniBranchCompany(admin.ModelAdmin):
+    pass
+
+@admin.register(Qualification)
+class AdmniQualification(admin.ModelAdmin):
+    pass
--- recomendador/settings.py
+++ recomendador/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'persons',
     "companies",
+    "services",
 ]
 
 MIDDLEWARE = [
@@ -70,7 +71,12 @@ TEMPLATES = [
 ]
 WSGI_APPLICATION = 'recomendador.wsgi.application'
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -85,16 +91,16 @@ WSGI_APPLICATION = 'recomendador.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'recomendador',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'recomendador',
+#         'USER': 'postgres',
+#         'PASSWORD': '123456',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
