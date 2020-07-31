7ccf2e6d960622181677428eed51a4199bda31cb
laura <laespaviza@gmail.com>
2020-07-31 17:31:34 -0500

    creacion admin client,superviser,city

--- persons/models.py
+++ persons/models.py
@@ -3,11 +3,23 @@ from django.db import models
 
 # Create your models here.
 
+class City(models.Model):
+    name = models.CharField(max_length=150)
+    department = models.CharField(max_length=150)
+
+    class Meta:
+        verbose_name = "Ciudad"
+        verbose_name_plural = "Ciudades"
+
+    def __str__(self):
+        return self.name
+
 class Client(models.Model):
     phone = models.BigIntegerField()
     address = models.CharField(max_length=255, blank=True)
     geolocation=models.CharField(max_length=255, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = "Cliente"
@@ -23,19 +35,6 @@ class Supervisor(models.Model):
     class Meta:
         verbose_name = "Supervisor"
         verbose_name_plural = "Supervisores"
-
-    def __str__(self):
-        return self.user.username
-
-
-class City(models.Model):
-    name=models.CharField(max_length=150)
-    department= models.CharField(max_length=150)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    class Meta:
-        verbose_name = "Ciudad"
-        verbose_name_plural = "Ciudades"
 
     def __str__(self):
         return self.user.username
